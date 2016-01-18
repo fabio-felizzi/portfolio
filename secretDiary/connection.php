378de9b794faac04f5872876e7a9d80ef2b20d0b
Fabio Felizzi <fabiofelizzi@gmail.com>
2016-01-18 18:32:29 +0000

    Removed unnccesary folders

--- secretDiary/connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "cl58-diary-ypq", "RhHfWHhw.", "cl58-diary-ypq");
-?>
--- secretDiary/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$(".contentContainer").css("min-height", $(window).height());
-	$("textarea").css("height", $(window).height()-110);
-	$("textarea").keyup(function() {
-		$.post("updateDiary.php", {diary:$("textarea").val()});
-	});
-
-});
