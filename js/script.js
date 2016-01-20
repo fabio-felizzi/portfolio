4617ff16e10784283d174d903124b69ae1457103
Fabio Felizzi <fabiofelizzi@gmail.com>
2016-01-20 21:21:43 +0000

    deleting files

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$(".page-content-wrapper").css("min-height", $(window).height());
-
-	$(".scrollTo").click(function(e) {
-        e.preventDefault();
-        $.scrollTo($(this).attr('href'), 1000);
-    });
-
-});
